0c8bb78de40147a35adfb75169d2ad15bc05c4bd
Alexey Ponomarev <alekseyponomarev@ozon.ru>
2024-05-20 11:16:57 +0300

    fonts and iterm2

--- install-macos.sh
+++ install-macos.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
 brew install zsh-syntax-highlighting powerlevel10k zfs zoxide thefuck fd
+brew install iterm2 tmux 
+brew install neovim
 
 git clone git@github.com:junegunn/fzf-git.sh.git
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
