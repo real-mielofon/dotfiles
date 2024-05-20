9f870e97f7327496ae0f37f704cc35af52b0afdb
Alexey Ponomarev <mielofon@gmail.com>
2024-05-20 12:07:47 +0300

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -9,5 +9,4 @@ brew install neovim
 git clone https://github.com/junegunn/fzf-git.sh.git
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
