3736cb7fb5cb57c3029591355c288e353cdec3c6
Alexey Ponomarev <mielofon@gmail.com>
2024-05-20 12:05:55 +0300

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -6,7 +6,7 @@ brew install zsh-syntax-highlighting powerlevel10k fzf zoxide thefuck fd eza
 brew install iterm2 tmux 
 brew install neovim
 
-git clone git@github.com:junegunn/fzf-git.sh.git
+git clone https://github.com/junegunn/fzf-git.sh.git
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
